65d6b7b6b706fac4a61ff864f614d6e531d50811
Justin Grimes <grimesajustin@gmail.com>
2021-05-27 21:22:27 -0400

    Add IBOutlet for dice2. Add IBAction for button. Hook up button to change dice values when tapped.

--- Projects/Dicee/Dicee/ViewController.swift
+++ Projects/Dicee/Dicee/ViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Set up the connection to the 2 dice images
     @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 4
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
     }
     
+    // Set up the connection to the Roll button
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+        
+        if (leftDiceNumber == 6){
+            leftDiceNumber = 0
+        }
+        
+        if (rightDiceNumber == -1) {
+            rightDiceNumber = 5
+        }
+    }
 }
